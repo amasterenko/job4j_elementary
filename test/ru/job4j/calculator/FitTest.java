ff7639a02c1a52d390835344969b59cd8fed2b19
Andrey Masterenko H <masterenko.as@gmail.com>
2020-07-24 21:58:51 +0300

    2. Тесты для идеальный вес [#330995]

---
+++ test/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FitTest {
+
+    @Test
+    public void whenManWeight180cm() {
+        short in = 180;
+        double expected = 92;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWomanWeight180cm() {
+        short in = 180;
+        double expected = 80.5;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.0);
+    }
+}
