81e178200a86e6badf55b80e1233fc7014615846
Andrey Masterenko H <masterenko.as@gmail.com>
2020-07-28 06:59:32 +0300

    6.0.2. Заполнение массива. [#330940]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Ivan";
+        names[1] = "Nikolay";
+        names[2] = "Andrey";
+        names[3] = "Fedor";
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
