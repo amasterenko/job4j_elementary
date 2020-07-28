4b694294b62c26df877557750e7599807c236cf2
Andrey Masterenko H <masterenko.as@gmail.com>
2020-07-28 06:30:34 +0300

    5.7. Ипотека [#331011: исправлено условие в цикле - вычисление переменной balance перенесено в тело цикло

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -2,9 +2,10 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
-        int year = 1;
+        int year = 0;
         double balance = amount;
-        while ((balance = balance * (1 + percent / 100) - salary) >= 0) {
+        while (balance > 0) {
+            balance = balance * (1 + percent / 100) - salary;
             year++;
         }
         return year;
