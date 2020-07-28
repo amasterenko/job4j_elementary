cec340121840ddc7dc593fc3802ff90fea74624e
Andrey Masterenko J <amasterenko@gmail.com>
2020-07-28 19:28:07 +0300

    Поиск индекса в диапазоне. [#330933]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -28,4 +28,49 @@ public class FindLoopTest {
         int result = FindLoop.indexOf(input, 7);
         assertThat(result, is(-1));
     }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 3, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNotFind3() {
+        int[] input = new int[] {3, 2, 10, 2, 4};
+        int value = 3;
+        int start = 1;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFinishLesStart() {
+        int[] input = new int[] {3, 2, 10, 2, 4};
+        int value = 3;
+        int start = 4;
+        int finish = 1;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayIsEmpty() {
+        int[] input = new int[] {};
+        int value = 3;
+        int start = 4;
+        int finish = 1;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
 }
