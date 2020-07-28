aac8c3b37fc5e1cde16cc5f8095d7a0a38c55454
Andrey Masterenko J <amasterenko@gmail.com>
2020-07-28 18:39:37 +0300

    6.4. Слово начинается с ... [#330928]: откорректировал условие проверки неверных аргументов метода

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        if (word.length == 0 || pref.length == 0) {
+        if (word.length == 0 || pref.length == 0 || pref.length > word.length) {
             return false;
         }
         for (int i = 0; i < pref.length; i++) {
