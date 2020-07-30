dadc230fd7deff5585d934e9711785ddb8d141c4
Andrey Masterenko H <masterenko.as@gmail.com>
2020-07-30 06:47:08 +0300

    6.8. Дефрагментация массива [#330950]: исправлен алгоритм - вызов метода перестановки элементов перенесен в условие поиска ненулевого элемента

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -5,15 +5,14 @@ public class Defragment {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
                 int point = index; /* указатель на null ячейку. */
-                int notNullIndex = index;
                 /* переместить первую не null ячейку. Нужен цикл. */
                 for (int j = index + 1; j < array.length; j++) {
                     if (array[j] != null) {
-                        notNullIndex = j;
+                        SwitchArray.swap(array, point, j);
                         break;
                     }
                 }
-                SwitchArray.swap(array, point, notNullIndex);
+
             }
             System.out.print(array[index] + " ");
         }
