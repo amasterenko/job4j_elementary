3bb6864dedbfd8384365019e8207735a16b9db88
Andrey Masterenko H <masterenko.as@gmail.com>
2020-08-05 07:52:30 +0300

    4. Переопределение [#331126]

---
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        return "{" + System.lineSeparator() + "\t\"name\"\t:" + "\t\"" + name + "\","
+                   + System.lineSeparator() + "\t\"body\"\t:" + "\t\"" + body + "\""
+                   + System.lineSeparator() + "}";
+    }
+}
