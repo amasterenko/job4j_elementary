fd78f777a4bf98a35e48af4ac6397999c9ed6eb5
Andrey Masterenko J <amasterenko@gmail.com>
2020-09-11 17:11:21 +0300

    5. Преобразование матрицы чисел в список чисел. [#330892]: метод конвертации через stream вынесен в отдельный класс

--- test/ru/job4j/stream/MatrixToListTest.java
+++ test/ru/job4j/stream/MatrixToListTest.java
@@ -11,15 +11,35 @@ import static org.junit.Assert.*;
 
 public class MatrixToListTest {
     @Test
-    public void whenListToMatrix() {
+    public void whenRectangularMatrixToList() {
         Integer[][] matrix = {
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}
         };
         List<Integer> expectedList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        assertThat(Arrays.stream(matrix)
-                .flatMap(e -> Stream.of(e))
-                .collect(Collectors.toList()), is(expectedList));
+        assertThat(MatrixToList.convert(matrix), is(expectedList));
+    }
+
+    @Test
+    public void whenNotRectangularMatrixToList() {
+        Integer[][] matrix = {
+                {1, 2, 3},
+                {4, 5},
+                {6}
+        };
+        List<Integer> expectedList = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertThat(MatrixToList.convert(matrix), is(expectedList));
+    }
+
+    @Test
+    public void whenNotRectangularMatrixWithEmptyRowToList1() {
+        Integer[][] matrix = {
+                {1, 2, 3},
+                {},
+                {6}
+        };
+        List<Integer> expectedList = Arrays.asList(1, 2, 3, 6);
+        assertThat(MatrixToList.convert(matrix), is(expectedList));
     }
 }
